be772756fbd9da35279b60048dd72ae0f32734f9
singharaj usai <djjd22938@gmail.com>
2022-09-20 18:55:20 -0400

    producttag column

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,39 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+//    ProductTag
+//id
+//Integer
+//Doesn't allow null values
+//Set as primary key
+//Uses auto increment
+id: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  primaryKey: true,
+  autoIncrement: true,
+},
+//product_id
+//Integer
+//References the product model's id
+product_id: {
+  type: DataTypes.INTEGER,
+references: {
+  key: 'id',
+  model: 'product',
+}
+},
+//tag_id
+//Integer
+//References the tag model's id
+tag_id: {
+  type: DataTypes.INTEGER,
+references: {
+  key: 'id',
+  model: 'tag',
+}
+},
+
   },
   {
     sequelize,
