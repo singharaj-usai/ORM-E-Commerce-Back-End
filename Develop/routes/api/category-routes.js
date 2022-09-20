508fcee2246661978a49eded8798bb2153fdd6d3
singharaj usai <djjd22938@gmail.com>
2022-09-20 19:48:04 -0400

    delete route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -52,12 +52,40 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// UPDATE a user
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update({category_name: req.body}, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// DELETE a user
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
