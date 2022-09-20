cd5d4771f6de532aceab90fdb1c429c233232c1b
singharaj usai <djjd22938@gmail.com>
2022-09-20 19:29:28 -0400

    find all categories route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,9 +3,20 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+//25-Ins_Literals
+
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+  const categoryData = await Category.findAll({
+    include: [{ model: Product }, { attributes: ['id','product_name','price','stock'] }],
+    attributes: ['id','category_name']
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
