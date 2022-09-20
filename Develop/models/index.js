bb58d30e77337b20d49aa9e83d69a448b6281df1
singharaj usai <djjd22938@gmail.com>
2022-09-20 19:06:10 -0400

    index.js wip

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,13 +3,28 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+//references 23-Ins_One-to-Many
 
 // Products belongsTo Category
-
+//Car.belongsTo(Driver, {
+//  foreignKey: 'driver_id',
+//});
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
+//Driver.hasMany(Car, {
+//  foreignKey: 'driver_id',
+//});
+//Look at 28-stu mini project too
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Product, {
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
