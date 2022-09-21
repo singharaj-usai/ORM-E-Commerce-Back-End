3975a62ff8c7c28e68dd345cea0678baece88afb
singharaj usai <djjd22938@gmail.com>
2022-09-20 20:27:27 -0400

    router put

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -17,13 +17,39 @@ router.get('/', async(req, res) => {
     }
   });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const categoryData = await Tag.findByPk({id: req.params.id}, {
+      include: [{ model: Product }, { attributes: ['id','product_name','price','stock'] }],
+      attributes: ['id','tag_name']
+      });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No driver found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try {
+    const categoryData = await Tag.create({tag_name: req.body},{where: {
+      id: req.params.id,
+    },
+  });
+    // 200 status code means the request is successful
+    res.status(200).json(categoryData);
+  } catch (err) {
+    // 400 status code means the server could not understand the request
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
