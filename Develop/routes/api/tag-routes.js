470beffde29ecb5f1ae414875f5061ee0b697eb2
singharaj usai <djjd22938@gmail.com>
2022-09-20 20:30:00 -0400

    router put

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -49,8 +49,22 @@ router.post('/', async(req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const categoryData = await Tag.update({tag_name: req.body.tag_name}, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
