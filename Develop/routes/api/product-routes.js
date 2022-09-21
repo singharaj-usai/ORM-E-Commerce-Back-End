b42f3deb1ec7cbd74814be4847739f8fb81283b5
singharaj usai <djjd22938@gmail.com>
2022-09-20 20:00:03 -0400

    product-routes get all

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,9 +4,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+  const categoryData = await Category.findAll({
+    include: [{ model: Category }, { attributes: ['id','category_name'] }, { model: Tag }, { attributes: ['id','tag_name'] }],
+    attributes: ['id','product_name','price','stock']
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
