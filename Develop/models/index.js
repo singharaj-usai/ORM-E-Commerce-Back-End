28551f846d65c631aec33b3e6a1c92b15ef54a9d
singharaj usai <djjd22938@gmail.com>
2022-11-02 13:04:45 -0400

    index.js exports fix

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,36 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 //references 23-Ins_One-to-Many
 
-// Products belongsTo Category
-//Car.belongsTo(Driver, {
-//  foreignKey: 'driver_id',
-//});
 Product.belongsTo(Category, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
 });
-// Categories have many Products
-//Driver.hasMany(Car, {
-//  foreignKey: 'driver_id',
-//});
 
 //Look at 28-stu mini project too
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'SET NULL'
 });
 
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Product, {
+Product.belongsToMany(Tag, {
   foreignKey: 'product_id',
   through: ProductTag,
-    //call this whatever you link - related to how the data will be returned (which key)
-    as: 'product_tags'
 });
-// Tags belongToMany Products (through ProductTag)
+
 Tag.belongsToMany(Product, {
   foreignKey: 'tag_id',
   through: ProductTag,
-    //call this whatever you link - related to how the data will be returned (which key)
-    as: 'product_tags'
 });
 
 module.exports = {
